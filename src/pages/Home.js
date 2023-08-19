5874d41dc116a763d7caa89c1b730ed29c2d7ecf
Aditya Goyal <aaddiittyyaagoyal@gmail.com>
2023-08-19 11:56:25 +0530

    second commit

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+const Home=()=>{
+    return (
+<div className='mb-20'>
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'> Hello, Welcome to my Blog</h1>
+    <p className='mx-auto leading-relaxed text-base mb-4'>
+    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+ Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+  A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+  It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+  Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic 
+  life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Gramma
+  </p>
+  <p className='mx-auto leading-relaxed text-base mb-4'>
+    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+ Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+  A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+  It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+  Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic 
+  life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Gramma
+  </p>
+  <p className='mx-auto leading-relaxed text-base mb-4'>
+    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+ Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+  A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+  It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+  Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic 
+  life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Gramma
+  </p>
+</div>
+    )
+}
+export default Home;
